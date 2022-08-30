12f4066dec5c23c5133df096affc64a59731ac77
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-08-30 11:51:07 -0500

    CReated limits in salary exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,23 @@
+use employees;
+# 2
+SELECT DISTINCT last_name
+FROM employees
+order by last_name desc
+limit 10;
+# 3
+SELECT DISTINCT emp_no
+FROM salaries
+where salary
+order by salary desc
+limit 5;
+# 4
+SELECT DISTINCT emp_no
+FROM salaries
+order by salary desc
+limit 4
+offset 46;
+
+
+
+
+
--- where_exercises.sql
+++ where_exercises.sql
@@ -1,27 +1,27 @@
 use employees;
 
-# select * from employees
-# where first_name IN ('Irena', 'Vidya', 'Maya');
+select * from employees
+where first_name IN ('Irena', 'Vidya', 'Maya');
 
-# select * from employees
-# where last_name LIKE 'E%';
+select * from employees
+where last_name LIKE 'E%';
 
-# select * from employees
-# where last_name LIKE '%q%';
+select * from employees
+where last_name LIKE '%q%';
 
-# select * from employees
-# where gender = 'M'
-# AND (first_name = 'Irena'
-#          OR first_name = 'Vidya'
-#          OR first_name = 'Maya');
+select * from employees
+where gender = 'M'
+AND (first_name = 'Irena'
+         OR first_name = 'Vidya'
+         OR first_name = 'Maya');
 
-# select * from employees
-# where last_name LIKE 'E%'
-#    OR last_name LIKE '%e';
+select * from employees
+where last_name LIKE 'E%'
+   OR last_name LIKE '%e';
 
-# select * from employees
-# where last_name LIKE 'E%'
-# AND last_name LIKE '%e';
+select * from employees
+where last_name LIKE 'E%'
+AND last_name LIKE '%e';
 
 select * from employees
 where last_name LIKE '%q%'
