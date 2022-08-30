7e599392665c57d04404c0b17672e239ceb2e7fe
Rogelio Lozano <lozano.rogelio92@gmail.com>
2022-08-30 14:35:07 -0500

    Created functions exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -7,7 +7,7 @@ order by first_name;
 select * from employees
 where first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY first_name , last_name;
-# 5
+# 4
 select * from employees
 where first_name IN ('Irena', 'Vidya', 'Maya')
 ORDER BY last_name , first_name;
@@ -21,22 +21,5 @@ where last_name LIKE 'E%'
 order by emp_no DESC;
 
 select * from employees
-where last_name LIKE '%q%';
-
-select * from employees
-where gender = 'M'
-  AND (first_name = 'Irena'
-    OR first_name = 'Vidya'
-    OR first_name = 'Maya');
-
-select * from employees
-where last_name LIKE 'E%'
-   OR last_name LIKE '%e';
-
-select * from employees
-where last_name LIKE 'E%'
-  AND last_name LIKE '%e';
-
-select * from employees
-where last_name LIKE '%q%'
-  AND last_name NOT LIKE '%qu%';
+where first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name desc , first_name desc;
